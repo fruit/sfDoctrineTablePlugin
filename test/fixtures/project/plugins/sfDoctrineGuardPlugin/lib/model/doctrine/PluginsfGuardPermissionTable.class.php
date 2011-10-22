dd32333a8125618ac19c8c899fdfb4de2741ea1f
Ilya Sabelnikov <fruit.dev@gmail.com>
2011-10-22 23:34:37 +0300

    Previous commit has added installed plugin models. Reverting changes to standart table state

--- test/fixtures/project/plugins/sfDoctrineGuardPlugin/lib/model/doctrine/PluginsfGuardPermissionTable.class.php
+++ test/fixtures/project/plugins/sfDoctrineGuardPlugin/lib/model/doctrine/PluginsfGuardPermissionTable.class.php
@@ -8,6 +8,6 @@
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
  * @version    SVN: $Id: PluginsfGuardPermissionTable.class.php 23793 2009-11-11 17:42:50Z Kris.Wallsmith $
  */
-abstract class PluginsfGuardPermissionTable extends Doctrine_Table_Example
+abstract class PluginsfGuardPermissionTable extends Doctrine_Table
 {
 }
